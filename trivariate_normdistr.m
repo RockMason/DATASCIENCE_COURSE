8f36080efa52d812142b5a7d4f064165c27e77b9
RockMason <rockamason@gmail.com>
2024-11-20 14:25:50 -0600

    Update trivariate_normdistr.m

--- trivariate_normdistr.m
+++ trivariate_normdistr.m
@@ -1,19 +1,25 @@
 %%Noise Lab 1 Trivariate Normal Distribution
 clear;
 close all;
-n = 10000;
+n = 1000;
 mu = zeros(3,1); %zero mean    
-X1 = randn(n,1);
-X2 = randn(n,1);
-X3 = randn(n,1);
-X = [X1 X2 X3];
-A = cov(X);
-fprintf('Covariance Matrix')
-display(A)
-B = corrcoef(A);
-fprintf('Correlation Coefficients')
-display(B)
+X1 = randn(1,n);
+X2 = randn(1,n);
+X3 = randn(1,n);
+X = [X1;X2;X3];
+A = randn(3,3);
+Y = A*X;
+figure
+plot3(X1,X2,X3,'.')
+figure
+plot3(Y(1,:),Y(2,:),Y(3,:),'.')
 
-Z = sqrtm(A).*X; 
-display(Z)
-plot3(Z(1),Z(2),Z(3));
+% fprintf('Covariance Matrix')
+% display(A)
+% B = corrcoef(A);
+% fprintf('Correlation Coefficients')
+% display(B)
+% 
+% Z = sqrtm(A).*X; 
+% display(Z)
+% plot3(Z(1),Z(2),Z(3));
